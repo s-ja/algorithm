6bb766e39319dc19e4f623f1cdca6c2c6862a7dc
s-ja <dkstmdwl0615@naver.com>
2023-11-11 22:32:15 +0900

    [PRG]양꼬치(120830)

--- Programmers/PRG120830/PRG120830.js
+++ Programmers/PRG120830/PRG120830.js
@@ -1,11 +1,26 @@
-function solution(numbers) {
+function solution(n, k) {
   var answer = 0;
-  numbers.forEach((element) => {
-    answer = answer + element;
-  });
-  answer = answer / numbers.length;
+  let a = n.toString();
+  a = Array.from(a);
+  a = a.slice(0, -1);
 
+  if (a.length === 3) {
+    let first = Number(a[0]) * 1000;
+    let second = Number(a[1]) * 100;
+    let third = Number(a[2]) * 10;
+    a = first + second + third;
+  } else if (a.length === 2) {
+    let first = Number(a[0]) * 100;
+    let second = Number(a[1]) * 10;
+    a = first + second;
+  } else if (a.length === 1) {
+    let first = Number(a[0]) * 10;
+    a = first;
+  }
+  let cola = a / 10;
+  k = k - cola;
+  answer = n * 12000 + k * 2000;
   return answer;
 }
 
-console.log(solution([89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]));
+console.log(solution(1, 3));
