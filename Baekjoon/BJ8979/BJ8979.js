277d24385ca79276bed877e5903e40c5bca88772
seungji <dkstmdwl0615@naver.com>
2023-11-13 02:33:23 +0900

    [BJ]올림픽(8979)

--- Baekjoon/BJ8979/BJ8979.js
+++ Baekjoon/BJ8979/BJ8979.js
@@ -1,39 +1,36 @@
-const { log } = require("console");
 const fs = require("fs");
-const { SourceMap } = require("module");
 let input = fs
   .readFileSync("Baekjoon/BJ8979/input.txt")
   .toString()
   .trim()
   .split("\n");
 
-const key = input.shift();
+const [N, K] = input.shift().split(" ").map(Number);
+const countries = input.map((line) => line.split(" ").map(Number));
 
-const countries = Number(key.split(" ")[0]);
-const countryNumber = Number(key.split(" ")[1]);
+console.log(countries);
 
-const medals = input.map((a) => a.split(" ").map(Number));
-
-medals.sort((a, b) => {
+countries.sort((a, b) => {
   if (a[1] !== b[1]) return b[1] - a[1];
   if (a[2] !== b[2]) return b[2] - a[2];
   return b[3] - a[3];
 });
 
+const KMedals = countries.find((country) => country[0] === Number(K));
+
 let rank = 1;
-for (let i = 0; i < medals.length; i++) {
-  if (medals[i][0] === countryNumber) {
-    break;
-  }
+for (let country of countries) {
   if (
-    i === 0 ||
-    medals[i][1] !== medals[i - 1][1] ||
-    medals[i][2] !== medals[i - 1][2] ||
-    medals[i][3] !== medals[i - 1][3]
+    country[1] > KMedals[1] ||
+    (country[1] === KMedals[1] && country[2] > KMedals[2]) ||
+    (country[1] === KMedals[1] &&
+      country[2] === KMedals[2] &&
+      country[3] > KMedals[3])
   ) {
-    rank = i + 1;
+    rank++;
+  } else if (country[0] === Number(K)) {
+    break;
   }
 }
 
-console.log(medals);
 console.log(rank);
