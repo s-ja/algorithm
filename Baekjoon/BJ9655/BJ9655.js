b08d8a338bc0d0c9c1d7b4d52d72eec173555f93
s-ja <dkstmdwl0615@naver.com>
2023-11-12 20:14:16 +0900

    [BJ]줄세우기(9655)

--- Baekjoon/BJ9655/BJ9655.js
+++ Baekjoon/BJ9655/BJ9655.js
@@ -1,13 +1,47 @@
 const fs = require("fs");
+let input = fs
+  .readFileSync(
+    "/Users/seungjian/Desktop/DEV/algorythm/Baekjoon/BJ9655/input.txt"
+  )
+  .toString()
+  .trim();
+input = input.split("\n");
 
-let input = fs.readFileSync("/dev/stdin");
+let inputCount = input[0];
+const inputTestCase = [];
 
-function solution(n) {
-  if (n % 2 === 0) {
-    console.log("CY");
-  } else {
-    console.log("SK");
+for (let i = 1; i <= inputCount; i++) {
+  const arr = input[i].split(" ").map((item) => +item);
+  //   console.log(arr);
+  let newArr = [];
+  for (let j = 1; j < arr.length; j++) {
+    newArr.push(arr[j]);
   }
+  const testCase = {
+    N: arr[0],
+    arr: newArr,
+  };
+  inputTestCase.push(testCase);
 }
 
-solution(input);
+// console.log(inputCount);
+// console.log(inputTestCase[0].arr);
+
+function solution(inputCount, inputTestCase) {
+  let step = 0;
+
+  function switchValues(arr, index1, index2) {
+    [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
+  }
+
+  for (i = 0; i <= inputCount; i++) {
+    for (j = 1; j <= inputTestCase[i].arr.length; j++) {
+      if (inputTestCase[i].arr[0] > inputTestCase[i].arr[1]) {
+        switchValues(inputTestCase[i].arr, 0, 1);
+      }
+    }
+  }
+  console.log(inputTestCase);
+}
+
+solution(inputCount, inputTestCase);
