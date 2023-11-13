2de1e0996936683d87618bffeea0f37602f83977
s-ja <dkstmdwl0615@naver.com>
2023-11-13 13:46:14 +0900

    [BJ]체스판 다시 칠하기(1018)

--- Baekjoon/BJ1018/BJ1018.js
+++ Baekjoon/BJ1018/BJ1018.js
@@ -9,32 +9,43 @@ const [N, M] = input.shift().split(" ").map(Number);
 
 // console.log(N, M);
 
-const board = [];
+// const board = [];
 
-for (let index = 0; index < input.length; index++) {
-  let row = input[index].split("");
-  board.push(row);
-}
+// for (let index = 0; index < input.length; index++) {
+//   let row = input[index].split("");
+//   board.push(row);
+// }
 
-// console.log(board);
+let board = [
+  ["W", "B", "W", "B", "W", "B", "W", "B"],
+  ["B", "W", "B", "W", "B", "W", "B", "W"],
+  ["W", "B", "W", "B", "W", "B", "W", "B"],
+  ["B", "W", "B", "B", "B", "W", "B", "W"],
+  ["W", "B", "W", "B", "W", "B", "W", "B"],
+  ["B", "W", "B", "W", "B", "W", "B", "W"],
+  ["W", "B", "W", "B", "W", "B", "W", "B"],
+  ["B", "W", "B", "W", "B", "W", "B", "W"],
+];
 
 // 1. 8*8일때 칠해야 하는 정사각형의 최소 개수를 구하는 함수
 
 let colorizeNumber = 0;
 
 function colorize(board) {
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[i].length; j++) {
-      if (board[i][j] === "W" && board[i][j] !== board[i][j + 1]) {
-        // board[i][j + 1] = "B";
+  let clone = board;
+  for (let i = 0; i < clone.length; i++) {
+    for (let j = 0; j < clone[i].length; j++) {
+      if (clone[i][j] !== clone[i][j + 1] && clone[i][j] === "W") {
         board[i].splice(j + 1, 1, "B");
-        colorizeNumber = colorizeNumber + 1;
-      } else if (board[i][j] === "B" && board[i][j] !== board[i][j + 1]) {
-        // board[i][j + 1] = "W";
-        board[i].splice(j + 1, 1, "W");
-        colorizeNumber = colorizeNumber + 1;
+        // colorizeNumber = colorizeNumber + 1;
+      } else if (clone[i][j] !== clone[i][j + 1] && clone[i][j] === "B") {
+        clone[i].splice(j + 1, 1, "W");
+        // colorizeNumber = colorizeNumber + 1;
+      } else {
+        break;
       }
     }
+    return clone;
   }
 }
 
