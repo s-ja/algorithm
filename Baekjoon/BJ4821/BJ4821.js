dbd26031408968f20d56a9f81df5dc8f43133f6c
s-ja <dkstmdwl0615@naver.com>
2023-11-13 20:28:09 +0900

    [BJ]페이지 세기(4821)

--- Baekjoon/BJ4821/BJ4821.js
+++ Baekjoon/BJ4821/BJ4821.js
@@ -7,15 +7,67 @@ let input = fs
 
 input.pop();
 
-// console.log(input);
-
-const obj = {};
-
-for (let i = 0; i < input.length; i += 2) {
-  const key = input[i].trim();
-  if (input[i + 1]) {
-    obj[key] = input[i + 1].split(","); // 값을 쉼표로 구분하여 배열로 변환
+function converter(input) {
+  const arr = [];
+  for (let i = 0; i < input.length; i += 2) {
+    const key = Number(input[i]);
+    if (input[i + 1]) {
+      const value = input[i + 1].split(",");
+      arr.push({ key, value });
+    }
   }
+  return arr;
 }
 
-console.log(obj);
+const data = converter(input);
+
+/*[
+  { key: 30, value: ["10-15", "25-28", "8-4", "13-20", "9", "8-8"] },
+  { key: 19, value: ["10-15", "25-28", "8-4", "13-20", "9", "8-8"] },
+]*/
+
+for (let index = 0; index < data.length; index++) {
+  let book = [];
+
+  for (let i = 1; i < data[index].key + 1; i++) {
+    book.push(i);
+  }
+
+  //   console.log(book);
+
+  let printRange = [];
+
+  for (let j = 0; j < data[index].value.length; j++) {
+    [first, second] = data[index].value[j].split("-");
+    first = Number(first);
+    second = Number(second);
+    if (!second) {
+      second = 0;
+    } else if (first === second) {
+      second = 0;
+    } else if (second - first < 0) {
+      first = 0;
+      second = 0;
+    }
+    //   console.log(first, second);
+    let partial = [];
+    for (let k = 0; k < second - first + 1; k++) {
+      partial.push(first + k);
+    }
+    if (!partial[0]) {
+      partial[0] = first;
+    }
+    printRange.push(...partial);
+  }
+
+  let uniqueArr = [];
+  printRange.forEach((element) => {
+    if (!uniqueArr.includes(element)) {
+      uniqueArr.push(element);
+    }
+  });
+
+  uniqueArr.sort((a, b) => a - b);
+  let intersection = book.filter((x) => uniqueArr.includes(x));
+  console.log(intersection.length);
+}
