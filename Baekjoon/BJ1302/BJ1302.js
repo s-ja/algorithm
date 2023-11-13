9a43d0eb5478f8a10af470f07152395151cffbca
s-ja <dkstmdwl0615@naver.com>
2023-11-13 22:50:32 +0900

    [BJ]베스트셀러(1302)

--- Baekjoon/BJ1302/BJ1302.js
+++ Baekjoon/BJ1302/BJ1302.js
@@ -13,16 +13,3 @@ console.log(input);
 
 let moreThan2 = input.filter((item, index) => input.indexOf(item) !== index);
 
-// console.log(moreThan2);
-
-const obj = {};
-
-for (let i = 0; i < moreThan2.length; i++) {
-  obj[moreThan2[i]] = 0;
-  for (let j = 0; j < input.length; j++) {
-    if (input[j] === moreThan2[i]) {
-    }
-  }
-}
-
-console.log(obj);
