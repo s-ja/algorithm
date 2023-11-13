80412db579b188d6796b9ddd785bf56a5e7e0c81
s-ja <dkstmdwl0615@naver.com>
2023-11-13 14:35:08 +0900

    [BJ]체스판 다시 칠하기(1018)

--- Baekjoon/BJ1018/BJ1018.js
+++ Baekjoon/BJ1018/BJ1018.js
@@ -9,22 +9,24 @@ const [N, M] = input.shift().split(" ").map(Number);
 
 // console.log(N, M);
 
-// const board = [];
+let board = [];
 
-// for (let index = 0; index < input.length; index++) {
-//   let row = input[index].split("");
-//   board.push(row);
-// }
+for (let index = 0; index < input.length; index++) {
+  let row = input[index].split("");
+  board.push(row);
+}
 
-let board = [
-  ["W", "B", "W", "B", "W", "B", "W", "B"],
-  ["B", "W", "B", "W", "B", "W", "B", "W"],
-  ["W", "B", "W", "B", "W", "B", "W", "B"],
-  ["B", "W", "B", "B", "B", "W", "B", "W"],
-  ["W", "B", "W", "B", "W", "B", "W", "B"],
-  ["B", "W", "B", "W", "B", "W", "B", "W"],
-  ["W", "B", "W", "B", "W", "B", "W", "B"],
-  ["B", "W", "B", "W", "B", "W", "B", "W"],
+board = [
+  ["B", "B", "B", "B", "B", "B", "B", "B", "W", "B", "W", "B", "W"],
+  ["B", "B", "B", "B", "B", "B", "B", "B", "B", "W", "B", "W", "B"],
+  ["B", "B", "B", "B", "B", "B", "B", "B", "W", "B", "W", "B", "W"],
+  ["B", "B", "B", "B", "B", "B", "B", "B", "B", "W", "B", "W", "B"],
+  ["B", "B", "B", "B", "B", "B", "B", "B", "W", "B", "W", "B", "W"],
+  ["B", "B", "B", "B", "B", "B", "B", "B", "B", "W", "B", "W", "B"],
+  ["B", "B", "B", "B", "B", "B", "B", "B", "W", "B", "W", "B", "W"],
+  ["B", "B", "B", "B", "B", "B", "B", "B", "B", "W", "B", "W", "B"],
+  ["W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "B", "W", "B"],
+  ["W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "B", "W", "B"],
 ];
 
 // 1. 8*8일때 칠해야 하는 정사각형의 최소 개수를 구하는 함수
@@ -32,26 +34,51 @@ let board = [
 let colorizeNumber = 0;
 
 function colorize(board) {
-  let clone = board;
-  for (let i = 0; i < clone.length; i++) {
-    for (let j = 0; j < clone[i].length; j++) {
-      if (clone[i][j] !== clone[i][j + 1] && clone[i][j] === "W") {
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < M; j++) {
+      if (board[i][j] !== board[i][j + 1] && board[i][j] === "W") {
         board[i].splice(j + 1, 1, "B");
-        // colorizeNumber = colorizeNumber + 1;
-      } else if (clone[i][j] !== clone[i][j + 1] && clone[i][j] === "B") {
-        clone[i].splice(j + 1, 1, "W");
-        // colorizeNumber = colorizeNumber + 1;
-      } else {
-        break;
+        colorizeNumber++;
+      } else if (board[i][j] !== board[i][j + 1] && board[i][j] === "B") {
+        board[i].splice(j + 1, 1, "W");
+        colorizeNumber++;
       }
     }
-    return clone;
   }
+  return board;
 }
 
-console.log(colorize(board), colorizeNumber);
+// console.log(colorize(board), colorizeNumber);
 
 // 1-1. 해당 블럭이 B일 경우에는 다음 블럭에 W를, W일 경우에는 B를 오게 해야 함.
 
 // board.length = row
 // board[i].length = column
+
+let row = [
+  "B",
+  "B",
+  "B",
+  "B",
+  "B",
+  "B",
+  "B",
+  "B",
+  "W",
+  "B",
+  "W",
+  "B",
+  "W",
+  "B",
+];
+
+let clone = [row[0]];
+for (let i = 0; i < row.length; i++) {
+  if (row[i] === row[i + 1] && row[i] === "W") {
+    clone.push("B");
+    colorizeNumber = colorizeNumber + 1;
+  } else if (row[i] === row[i + 1] && row[i] === "B") {
+    clone.push("W");
+    colorizeNumber = colorizeNumber + 1;
+  }
+}
