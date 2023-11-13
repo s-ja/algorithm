1c9ff69e6f7cdeaf48fd7e16fa0e4b75688cfda5
s-ja <dkstmdwl0615@naver.com>
2023-11-13 12:59:44 +0900

    [BJ]체스판 다시 칠하기(1018)

--- Baekjoon/BJ1018/BJ1018.js
+++ Baekjoon/BJ1018/BJ1018.js
@@ -5,4 +5,42 @@ let input = fs
   .trim()
   .split("\n");
 
-console.log(input);
+const [N, M] = input.shift().split(" ").map(Number);
+
+// console.log(N, M);
+
+const board = [];
+
+for (let index = 0; index < input.length; index++) {
+  let row = input[index].split("");
+  board.push(row);
+}
+
+// console.log(board);
+
+// 1. 8*8일때 칠해야 하는 정사각형의 최소 개수를 구하는 함수
+
+let colorizeNumber = 0;
+
+function colorize(board) {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (board[i][j] === "W" && board[i][j] !== board[i][j + 1]) {
+        // board[i][j + 1] = "B";
+        board[i].splice(j + 1, 1, "B");
+        colorizeNumber = colorizeNumber + 1;
+      } else if (board[i][j] === "B" && board[i][j] !== board[i][j + 1]) {
+        // board[i][j + 1] = "W";
+        board[i].splice(j + 1, 1, "W");
+        colorizeNumber = colorizeNumber + 1;
+      }
+    }
+  }
+}
+
+console.log(colorize(board), colorizeNumber);
+
+// 1-1. 해당 블럭이 B일 경우에는 다음 블럭에 W를, W일 경우에는 B를 오게 해야 함.
+
+// board.length = row
+// board[i].length = column
