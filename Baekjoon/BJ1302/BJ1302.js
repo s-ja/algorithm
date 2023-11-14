4c065b79683b4a33998af709ac6be21ff1a45125
s-ja <dkstmdwl0615@naver.com>
2023-11-14 12:25:45 +0900

    [BJ]베스트셀러(1302)

--- Baekjoon/BJ1302/BJ1302.js
+++ Baekjoon/BJ1302/BJ1302.js
@@ -7,9 +7,53 @@ let input = fs
 
 let soldNumber = input.shift();
 
-console.log(soldNumber);
+// console.log(soldNumber);
 
-console.log(input);
+// console.log(input);
 
 let moreThan2 = input.filter((item, index) => input.indexOf(item) !== index);
 
+// console.log(moreThan2);
+
+let uniqueArr = [];
+moreThan2.forEach((element) => {
+  if (!uniqueArr.includes(element)) {
+    uniqueArr.push(element);
+  }
+});
+
+// 1. 오늘 판매된 책들 중 2권 이상 팔린 책들의 목록을 만든다.
+// 2. 오늘 판매된 책들 중 2권 이상 팔린 책의 목록 안에 있는 책들의 판매 갯수를 산출한다.
+// 3. 판매된 갯수가 동일하다면 사전순으로 정렬하여 가장 앞에 있는 책을 반환한다.
+
+let test = [];
+for (let j = 0; j < uniqueArr.length; j++) {
+  const testCase = {
+    title: uniqueArr[j],
+    count: 0,
+  };
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === uniqueArr[j]) {
+      testCase.count++;
+    }
+  }
+  test.push(testCase);
+}
+
+// console.log(test);
+
+let testFirst = test[0];
+
+for (let k = 0; k < test.length; k++) {
+  if (!test[k + 1]) {
+    break;
+  } else if (test[k].count < test[k + 1].count) {
+    testFirst = test[k + 1];
+  } else if (test[k].count === test[k + 1].count) {
+    if (test[k].title < test[k + 1].title) {
+      testFirst = test[k];
+    }
+  }
+}
+
+console.log(testFirst.title);
