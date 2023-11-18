572f4e5ce75d8d99be3ed381b59a47cb6c4ef9dd
s-ja <dkstmdwl0615@naver.com>
2023-11-18 15:33:04 +0900

    [BJ]체스판 다시 칠하기(1018)

--- Baekjoon/BJ1018/BJ1018.js
+++ Baekjoon/BJ1018/BJ1018.js
@@ -7,7 +7,7 @@ let input = fs
 
 const [N, M] = input.shift().split(" ").map(Number);
 
-console.log(N, M);
+// console.log(N, M);
 // N = row
 // M = column
 
@@ -18,7 +18,7 @@ for (let index = 0; index < input.length; index++) {
   board.push(row);
 }
 
-console.log(board);
+// console.log(board);
 
 // board = [
 //   ["B", "B", "B", "B", "B", "B", "B", "B", "W", "B", "W", "B", "W"],
@@ -41,10 +41,29 @@ console.log(board);
 
 let data = [];
 
-for (let i = 0; i < board.length - 8; i++) {
-  let chessBoard = [];
-  let test = board.slice(i, i + 8);
-  for (let j = 0; j < board[i].length - 8; j++) {
-    chessBoard.push(test[])
+let minPaint = Infinity;
+
+for (let i = 0; i <= board.length - 8; i++) {
+  for (let j = 0; j <= board[i].length - 8; j++) {
+    minPaint = Math.min(
+      minPaint,
+      countPaints(board, i, j, "W"),
+      countPaints(board, i, j, "B")
+    );
   }
+}
+console.log(minPaint);
+
+function countPaints(board, x, y, color) {
+  let paintCount = 0;
+  for (let i = x; i < x + 8; i++) {
+    for (let j = y; j < y + 8; j++) {
+      if (board[i][j] !== color) {
+        paintCount++;
+      }
+      color = color === "W" ? "B" : "W";
+    }
+    color = color === "W" ? "B" : "W";
+  }
+  return paintCount;
 }
