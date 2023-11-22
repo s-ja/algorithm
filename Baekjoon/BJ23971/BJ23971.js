b044444abd4b5bcfd480bd7376f2c348aff946ce
s-ja <dkstmdwl0615@naver.com>
2023-11-22 13:14:32 +0900

    [BJ]ZOAC 4(23971)

---
+++ Baekjoon/BJ23971/BJ23971.js
@@ -0,0 +1,33 @@
+const { count } = require("console");
+const fs = require("fs");
+let input = fs
+  .readFileSync("Baekjoon/BJ23971/input.txt")
+  .toString()
+  .trim()
+  .split(" ");
+
+console.log(input);
+
+// input = ["5", "4", "1", "1"];
+
+const H = Number(input[0]);
+const W = Number(input[1]);
+const N = Number(input[2]);
+const M = Number(input[3]);
+
+console.log(H, W, N, M);
+
+// let col = new Array(W).fill(0);
+// let row = new Array(H).fill(0);
+
+// console.log(col, row);
+
+let countPlace = 0;
+
+for (let i = 0; i < H; i = i + N+1) {
+  for (let j = 0; j < W; j = j + M+1) {
+    countPlace++;
+  }
+}
+
+console.log(countPlace);
