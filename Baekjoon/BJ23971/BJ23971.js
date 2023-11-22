658ca1b2e63b3b1a42b1bdda689f1a9600b611d0
s-ja <dkstmdwl0615@naver.com>
2023-11-22 13:51:55 +0900

    [BJ]ZOAC 4(23971)

--- Baekjoon/BJ23971/BJ23971.js
+++ Baekjoon/BJ23971/BJ23971.js
@@ -6,7 +6,7 @@ let input = fs
   .trim()
   .split(" ");
 
-console.log(input);
+// console.log(input);
 
 // input = ["5", "4", "1", "1"];
 
@@ -15,19 +15,24 @@ const W = Number(input[1]);
 const N = Number(input[2]);
 const M = Number(input[3]);
 
-console.log(H, W, N, M);
+// console.log(H, W, N, M);
 
 // let col = new Array(W).fill(0);
 // let row = new Array(H).fill(0);
 
 // console.log(col, row);
 
-let countPlace = 0;
+// let countPlace = 0;
 
-for (let i = 0; i < H; i = i + N+1) {
-  for (let j = 0; j < W; j = j + M+1) {
-    countPlace++;
-  }
-}
+// for (let i = 0; i < H; i = i + N + 1) {
+//   for (let j = 0; j < W; j = j + M + 1) {
+//     countPlace++;
+//   }
+// }
+
+let rowCount = Math.floor((H + N) / (N + 1));
+let colCount = Math.floor((W + M) / (M + 1));
+
+let countPlace = rowCount * colCount;
 
 console.log(countPlace);
