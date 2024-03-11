4a93a3ba6fdcd6c928e6e776d08b2c4fea127b15
an seungji <ansj8777@gmail.com>
2024-03-11 20:28:33 +0900

    [PRG]문자열 뒤집기(120822)

--- Programmers/PRG120822/PRG120822.js
+++ Programmers/PRG120822/PRG120822.js
@@ -1,8 +1,19 @@
 function solution(my_string) {
-  var answer = "";
+  let arr = my_string.split("");
+  let result = [];
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    result.push(arr[i]);
+  }
+
+  let answer = "";
+
+  for (elem of result) {
+    answer += elem;
+  }
+
   return answer;
 }
-
 
 console.log(solution("jaron"));
 console.log(solution("bread"));
