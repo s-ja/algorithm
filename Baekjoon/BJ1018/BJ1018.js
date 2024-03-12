5e30de9f2684ff83edc785b8a4a1f65297aa07d2
an seungji <ansj8777@gmail.com>
2024-03-12 13:42:48 +0900

    [BJ]체스판 다시 칠하기(1018)

--- Baekjoon/BJ1018/BJ1018.js
+++ Baekjoon/BJ1018/BJ1018.js
@@ -7,63 +7,33 @@ let input = fs
 
 const [N, M] = input.shift().split(" ").map(Number);
 
-// console.log(N, M);
-// N = row
-// M = column
+const board = [];
 
-let board = [];
-
-for (let index = 0; index < input.length; index++) {
-  let row = input[index].split("");
-  board.push(row);
+for (elem of input) {
+  elem = elem.replace(/\r/g, "");
+  board.push(elem.split(""));
 }
 
-// console.log(board);
+// console.log(N,M)
+// console.log(board)
 
-// board = [
-//   ["B", "B", "B", "B", "B", "B", "B", "B", "W", "B", "W", "B", "W"],
-//   ["B", "B", "B", "B", "B", "B", "B", "B", "B", "W", "B", "W", "B"],
-//   ["B", "B", "B", "B", "B", "B", "B", "B", "W", "B", "W", "B", "W"],
-//   ["B", "B", "B", "B", "B", "B", "B", "B", "B", "W", "B", "W", "B"],
-//   ["B", "B", "B", "B", "B", "B", "B", "B", "W", "B", "W", "B", "W"],
-//   ["B", "B", "B", "B", "B", "B", "B", "B", "B", "W", "B", "W", "B"],
-//   ["B", "B", "B", "B", "B", "B", "B", "B", "W", "B", "W", "B", "W"],
-//   ["B", "B", "B", "B", "B", "B", "B", "B", "B", "W", "B", "W", "B"],
-//   ["W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "B", "W", "B"],
-//   ["W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "B", "W", "B"],
-// ];
+const test = []
 
-// 주어진 board 에서 나올 수 있는 8*8 체스판의 모든 경우를 구한다.
-
-// 각 체스판 경우들에서 칠해야 하는 횟수를 구한다.
-
-// 칠해야 하는 가장 작은 횟수를 반환한다.
-
-let data = [];
-
-let minPaint = Infinity;
-
-for (let i = 0; i <= board.length - 8; i++) {
-  for (let j = 0; j <= board[i].length - 8; j++) {
-    minPaint = Math.min(
-      minPaint,
-      countPaints(board, i, j, "W"),
-      countPaints(board, i, j, "B")
-    );
+for (let i = 0; i <= N - 8; i++) {
+    for (let j = 0; j <= M - 8; j++) {
+        
   }
 }
-console.log(minPaint);
 
-function countPaints(board, x, y, color) {
-  let paintCount = 0;
-  for (let i = x; i < x + 8; i++) {
-    for (let j = y; j < y + 8; j++) {
-      if (board[i][j] !== color) {
-        paintCount++;
-      }
-      color = color === "W" ? "B" : "W";
-    }
-    color = color === "W" ? "B" : "W";
-  }
-  return paintCount;
-}
+[
+  ["B", "B", "B", "B", "B", "B", "B", "B", "B", "B"],
+  ["B", "B", "W", "B", "W", "B", "W", "B", "W", "B"],
+  ["B", "W", "B", "W", "B", "W", "B", "W", "B", "B"],
+  ["B", "B", "W", "B", "W", "B", "W", "B", "W", "B"],
+  ["B", "W", "B", "W", "B", "W", "B", "W", "B", "B"],
+  ["B", "B", "W", "B", "W", "B", "W", "B", "W", "B"],
+  ["B", "W", "B", "W", "B", "W", "B", "W", "B", "B"],
+  ["B", "B", "W", "B", "W", "B", "W", "B", "W", "B"],
+  ["B", "W", "B", "W", "B", "W", "B", "W", "B", "B"],
+  ["B", "B", "B", "B", "B", "B", "B", "B", "B", "B"],
+];
