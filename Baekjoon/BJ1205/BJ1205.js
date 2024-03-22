8cff60d92f592ea5660bf0b33d3897f0381e8376
an seungji <ansj8777@gmail.com>
2024-03-22 16:02:02 +0900

    [BJ]등수 구하기(1205)

--- Baekjoon/BJ1205/BJ1205.js
+++ Baekjoon/BJ1205/BJ1205.js
@@ -1,8 +1,8 @@
 const { fail } = require("assert");
 const fs = require("fs");
 let input = fs
-  .readFileSync("Baekjoon/BJ1205/input.txt")
-  //   .readFileSync("/dev/stdin")
+  //   .readFileSync("Baekjoon/BJ1205/input.txt")
+  .readFileSync("/dev/stdin")
   .toString()
   .trim()
   .split("\n");
@@ -13,31 +13,24 @@ let input = fs
 
 //둘째 줄에는 현재 랭킹 리스트에 있는 점수가 비오름차순으로 주어진다.
 
-const [N, score, P] = input[0].split(" ").map(Number);
-const final = [];
+const [n, score, p] = input[0].split(" ").map(Number);
 
-if (N) {
-  const rank = input[1].split(" ").map(Number);
+if (n) {
+  const arr = input[1].split(" ").map(Number);
   //   console.log(rank);
-  for (let i = 0; i < P; i++) {
-    if (rank[i]) {
-      if (rank[i] > score) {
-        final[i] = rank[i];
-      } else {
-        final[i] = score;
-      }
-    } else {
-      final[i] = score;
-      break;
-    }
+  arr.push(score);
+  const arrSorted = arr.sort((a, b) => b - a);
+  console.log(arrSorted);
+  let indexes = [];
+  let index = arrSorted.indexOf(score);
+
+  while (index !== -1) {
+    indexes.push(index + 1);
+    index = arrSorted.indexOf(score, index + 1);
   }
 
-  if (N === P && score === rank[P - 1]) {
-    console.log(-1);
-  } else {
-    console.log(final.indexOf(score) + 1);
-  }
-  //   console.log(final);
+  if (indexes[indexes.length - 1] > p) console.log(-1);
+  else console.log(indexes[0]);
 } else {
   console.log(1);
 }
