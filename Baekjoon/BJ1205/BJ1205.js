dc041ee91debcfe75b35720dc3f78b5d1c1c2b47
an seungji <ansj8777@gmail.com>
2024-03-22 15:30:35 +0900

    [BJ]등수 구하기(1205)

--- Baekjoon/BJ1205/BJ1205.js
+++ Baekjoon/BJ1205/BJ1205.js
@@ -1,3 +1,4 @@
+const { fail } = require("assert");
 const fs = require("fs");
 let input = fs
   .readFileSync("Baekjoon/BJ1205/input.txt")
@@ -13,26 +14,27 @@ let input = fs
 //둘째 줄에는 현재 랭킹 리스트에 있는 점수가 비오름차순으로 주어진다.
 
 const [N, score, P] = input[0].split(" ").map(Number);
+const final = Array.from({ length: P }, () => 0);
 
-console.log(N, score, P);
-console.log(input);
-
-if (input[1]) {
+if (N) {
   const rank = input[1].split(" ").map(Number);
-
-  console.log(rank);
-
-  let final = [];
-
+  //   console.log(rank);
   for (let i = 0; i < P; i++) {
-    if (rank[i] > score) {
-      final.push(rank[i]);
+    if (rank[i]) {
+      if (rank[i] > score) {
+        final[i] = rank[i];
+      } else {
+        final[i] = score;
+      }
     } else {
-      final.push(score);
+      final[i] = score;
+      break;
     }
   }
 
-  if (score === rank[P - 1]) {
+  console.log(final);
+
+  if (final.indexOf(score) === rank.indexOf(score)) {
     console.log(-1);
   } else {
     console.log(final.indexOf(score) + 1);
