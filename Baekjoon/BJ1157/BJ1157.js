2c1eaac0ed124f4f605e22f9961b32d6500bbe02
an seungji <ansj8777@gmail.com>
2025-12-09 15:15:23 +0900

    모각코 알고리즘 스터디 2주차: 문자열

--- Baekjoon/BJ1157/BJ1157.js
+++ Baekjoon/BJ1157/BJ1157.js
@@ -9,11 +9,11 @@ const wordCount = {};
 for (let i = 0; i < input.length; i++) {
   wordCount[input[i].toUpperCase()] = (wordCount[input[i].toUpperCase()] || 0) + 1;
 }
-console.log(wordCount);
+// console.log(wordCount);
 
 // 각 알파벳의 갯수를 비교하여 가장 많은 갯수를 찾는다.
 const maxCount = Math.max(...Object.values(wordCount));
-console.log(maxCount);
+// console.log(maxCount);
 
 // 가장 많은 갯수의 알파벳을 찾는다.
 const maxCountLetter = []
@@ -22,7 +22,7 @@ for (let key in wordCount) {
         maxCountLetter.push(key);
     }
 }
-console.log(maxCountLetter);
+// console.log(maxCountLetter);
 
 // 가장 많은 알파벳이 여러 개인 경우에는 ?를 출력한다.
 if (Object.values(wordCount).filter((count) => count === maxCount).length > 1) {
