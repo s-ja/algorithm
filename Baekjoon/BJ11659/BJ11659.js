51427c2ee4b9a9895baca11859591be85d9bac24
an seungji <ansj8777@gmail.com>
2025-12-28 01:34:19 +0900

    모각코 알고리즘 스터디 4주차: 수학(소수, GCD/LCM), 누적합

--- Baekjoon/BJ11659/BJ11659.js
+++ Baekjoon/BJ11659/BJ11659.js
@@ -8,12 +8,12 @@ const input = fs
 // 백준 제출용 (표준 입력 사용)
 // const input = fs.readFileSync(0, "utf-8").toString().trim().split("\n");
 
-const [N, M] = input[0].split(" ").map(Number);
-const numbers = input[1].split(" ").map(Number);
+const [N, M] = input.shift().split(" ").map(Number);
+const numbers = input.shift().split(" ").map(Number);
 const results = [];
 
 for (let i = 0; i < M; i++) {
-  const [start, end] = input[i + 2].split(" ").map(Number);
+  const [start, end] = input.shift().split(" ").map(Number);
   results.push(
     numbers.slice(start - 1, end).reduce((acc, curr) => acc + curr, 0)
   );
