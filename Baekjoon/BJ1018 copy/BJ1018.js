4bff025e15f57fe541d8920a88f59678e39bd3e5
an seungji <ansj8777@gmail.com>
2026-01-02 22:53:24 +0900

    모각코 알고리즘 스터디 5주 (12/29~1/4): 완전탐색, 그리디 기초

--- Baekjoon/BJ1018 copy/BJ1018.js
+++ Baekjoon/BJ1018 copy/BJ1018.js
@@ -16,23 +16,6 @@ for (elem of input) {
   board.push(elem.split(""));
 }
 
-// console.log(board);
-// const boardLog = [
-//   ["B", "B", "B", "B", "B", "B", "B", "B", "B", "B"],
-//   ["B", "B", "W", "B", "W", "B", "W", "B", "W", "B"],
-//   ["B", "W", "B", "W", "B", "W", "B", "W", "B", "B"],
-//   ["B", "B", "W", "B", "W", "B", "W", "B", "W", "B"],
-//   ["B", "W", "B", "W", "B", "W", "B", "W", "B", "B"],
-//   ["B", "B", "W", "B", "W", "B", "W", "B", "W", "B"],
-//   ["B", "W", "B", "W", "B", "W", "B", "W", "B", "B"],
-//   ["B", "B", "W", "B", "W", "B", "W", "B", "W", "B"],
-//   ["B", "W", "B", "W", "B", "W", "B", "W", "B", "B"],
-//   ["B", "B", "B", "B", "B", "B", "B", "B", "B", "B"],
-// ];
-
-// i = column
-// j = row
-
 const testCases = [];
 const paintedCounts = [];
 
@@ -43,142 +26,23 @@ for (let i = 0; i <= N - 8; i++) {
   }
   testCases.push(testCase);
 }
-
-// console.log(JSON.stringify(testCases, null, 2));
-
-// testCases 배열의 0 번째 인자 = x 를 0 으로 뒀을 때, 만들 수 있는 모든 8 * 8 체스판 조합 = 10 기준 3개
-
-// const testCasesLog = [
-//   [
-//     [
-//       ["B", "B", "B", "B", "B", "B", "B", "B"],
-//       ["B", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "W"],
-//       ["B", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "W"],
-//       ["B", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "W"],
-//       ["B", "B", "W", "B", "W", "B", "W", "B"],
-//     ],
-//     [
-//       ["B", "B", "B", "B", "B", "B", "B", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "W"],
-//       ["W", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "W"],
-//       ["W", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "W"],
-//       ["W", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "W"],
-//     ],
-//     [
-//       ["B", "B", "B", "B", "B", "B", "B", "B"],
-//       ["W", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "B"],
-//       ["W", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "B"],
-//       ["W", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "B"],
-//       ["W", "B", "W", "B", "W", "B", "W", "B"],
-//     ],
-//   ],
-//   [
-//     [
-//       ["B", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "W"],
-//       ["B", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "W"],
-//       ["B", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "W"],
-//       ["B", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "W"],
-//     ],
-//     [
-//       ["B", "W", "B", "W", "B", "W", "B", "W"],
-//       ["W", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "W"],
-//       ["W", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "W"],
-//       ["W", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "W"],
-//       ["W", "B", "W", "B", "W", "B", "W", "B"],
-//     ],
-//     [
-//       ["W", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "B"],
-//       ["W", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "B"],
-//       ["W", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "B"],
-//       ["W", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "B"],
-//     ],
-//   ],
-//   [
-//     [
-//       ["B", "W", "B", "W", "B", "W", "B", "W"],
-//       ["B", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "W"],
-//       ["B", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "W"],
-//       ["B", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "W"],
-//       ["B", "B", "B", "B", "B", "B", "B", "B"],
-//     ],
-//     [
-//       ["W", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "W"],
-//       ["W", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "W"],
-//       ["W", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "W"],
-//       ["W", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "B", "B", "B", "B", "B", "B", "B"],
-//     ],
-//     [
-//       ["B", "W", "B", "W", "B", "W", "B", "B"],
-//       ["W", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "B"],
-//       ["W", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "B"],
-//       ["W", "B", "W", "B", "W", "B", "W", "B"],
-//       ["B", "W", "B", "W", "B", "W", "B", "B"],
-//       ["B", "B", "B", "B", "B", "B", "B", "B"],
-//     ],
-//   ],
-// ];
-
 for (let i = 0; i < testCases.length; i++) {
   for (let j = 0; j < testCases[i].length; j++) {
     const thisBoard = testCases[i][j];
-    let paintedCount = painter2(thisBoard);
-    paintedCounts.push(paintedCount);
+    const countB = painter(thisBoard, "B");
+    const countW = painter(thisBoard, "W");
+    paintedCounts.push(Math.min(countB, countW));
   }
 }
 
 console.log(paintedCounts);
 console.log(Math.min(...paintedCounts));
 
-function painter(board) {
+function painter(board, startColor) {
   let paintedCount = 0;
-  let color = board[0][0];
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[i].length; j++) {
-      if (board[i][j] !== color) {
-        paintedCount++;
-      }
-      color = color === "B" ? "W" : "B";
-    }
-  }
-  return paintedCount;
-}
-
-function painter2(board) {
-  let paintedCount = 0;
-  const startColor = board[0][0];
 
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length; j++) {
-      // 체스판 패턴: (i+j)가 짝수면 시작 색, 홀수면 반대 색
       const shouldBeStartColor = (i + j) % 2 === 0;
       const expectedColor = shouldBeStartColor
         ? startColor
@@ -194,16 +58,3 @@ function painter2(board) {
 
   return paintedCount;
 }
-
-const testBoard = [
-  ["W", "W", "B", "W", "B", "W", "B", "B"], // 6
-  ["W", "B", "W", "B", "W", "B", "W", "B"], // 8
-  ["B", "W", "B", "W", "B", "W", "B", "B"], // 7
-  ["W", "B", "W", "B", "W", "B", "W", "B"], // 8
-  ["B", "W", "B", "W", "B", "W", "B", "B"], // 7
-  ["W", "B", "W", "B", "W", "B", "W", "B"], // 8
-  ["B", "W", "B", "W", "B", "W", "B", "B"], // 7
-  ["B", "B", "B", "B", "B", "B", "B", "B"], // 4
-];
-// console.log(painter(testBoard)); // 6 + 8 + 7 + 8 + 7 + 8 + 7 + 4 = 55
-// console.log(painter2(testBoard)); // 6 + 8 + 7 + 8 + 7 + 8 + 7 + 4 = 55
