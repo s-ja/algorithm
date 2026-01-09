a1a73394c2d6e90f7cb8bea36f79eddc523c7c36
asj <ansj8777@gmail.com>
2026-01-09 12:50:27 +0900

    5주차

---
+++ Baekjoon/BJ23968/BJ23968.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+// 로컬 테스트용 (input.txt 파일 사용)
+const input = fs
+  .readFileSync("Baekjoon/BJ23968/input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+// 백준 제출용 (표준 입력 사용)
+// const input = fs.readFileSync(0, "utf-8").toString().trim().split("\n");
+
+const [N, K] = input.shift().split(" ").map(Number);
+
+const numbers = input.shift().split(" ").map(Number);
+
+console.log(numbers);
