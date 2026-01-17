68f44167013233c096d6397b7da67c1a8b4effff
an seungji <ansj8777@gmail.com>
2026-01-17 23:40:41 +0900

    7주 (1/12~1/18) : 이진 탐색 (Binary Search), 합병 정렬(Merge Sort): 5번 문제

--- Baekjoon/BJ24062/BJ24062.js
+++ Baekjoon/BJ24062/BJ24062.js
@@ -12,13 +12,13 @@ const N = Number(input[0]);
 const A = input[1].split(" ").map(Number);
 const B = input[2].split(" ").map(Number);
 
+let found = false;
 
 function checkIfEqual(A) {
   for (let i = 0; i < N; i++) {
     if (A[i] !== B[i]) return;
   }
-  console.log(1);
-  process.exit(0);
+  found = true;
 }
 
 function merge_sort(A, p, r) {
@@ -33,7 +33,7 @@ function merge_sort(A, p, r) {
 function merge(A, p, q, r) {
   let i = p;
   let j = q + 1;
-  let t = 1;
+  let t = 0;
   const tmp = [];
   while (i <= q && j <= r) {
     if (A[i] <= A[j]) {
@@ -49,11 +49,16 @@ function merge(A, p, q, r) {
     tmp[t++] = A[j++];
   }
   i = p;
-  t = 1;
+  t = 0;
   while (i <= r) {
     A[i++] = tmp[t++];
     checkIfEqual(A);
   }
+
+  if (found) {
+  console.log(1);
+  process.exit(0);
+}
 }
 
 merge_sort(A, 0, N - 1);
