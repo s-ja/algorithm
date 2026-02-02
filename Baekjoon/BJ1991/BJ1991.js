2e0bfbc6cce0563bfa3ecef645424805e15692de
an seungji <ansj8777@gmail.com>
2026-02-02 12:15:09 +0900

    10주 (2/2~2/8): 트리 기초 (순회, BST): 1번 문제

--- Baekjoon/BJ1991/BJ1991.js
+++ Baekjoon/BJ1991/BJ1991.js
@@ -6,9 +6,13 @@
 const fs = require("fs");
 // 로컬 테스트용 (input.txt 파일 사용)
 const input = fs
-  .readFileSync("Baekjoon/BJ1991/input.txt")
+  .readFileSync("BJ1991/input.txt")
   .toString()
   .trim()
   .split("\n");
 // 백준 제출용 (표준 입력 사용)
 // const input = fs.readFileSync(0, "utf-8").toString().trim().split("\n");
+
+const N = Number(input[0]);
+
+console.log(N)
