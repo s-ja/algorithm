c36f912d773b80d2c3876a718ca97e8b343cd310
an seungji <ansj8777@gmail.com>
2026-02-07 20:47:36 +0900

    10주 (2/2~2/8): 트리 기초 (순회, BST): 2번 문제

---
+++ Baekjoon/BJ1539/BJ1539.js
@@ -0,0 +1,14 @@
+/**
+ * 문제 번호: BJ1539
+ * 문제 링크: https://www.acmicpc.net/problem/1539
+ */
+
+const fs = require("fs");
+// 로컬 테스트용 (input.txt 파일 사용)
+const input = fs
+  .readFileSync("Baekjoon/BJ1539/input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+// 백준 제출용 (표준 입력 사용)
+// const input = fs.readFileSync(0, "utf-8").toString().trim().split("\n");
