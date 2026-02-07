1c7c0fa95241fedff348e5bddda02722196873e1
an seungji <ansj8777@gmail.com>
2026-02-07 19:14:15 +0900

    10주 (2/2~2/8): 트리 기초 (순회, BST): 1번 문제

--- Baekjoon/BJ1991/BJ1991.js
+++ Baekjoon/BJ1991/BJ1991.js
@@ -15,4 +15,49 @@ const input = fs
 
 const N = Number(input[0]);
 
-console.log(N)
+const tree = {};
+
+for (let i = 1; i <= N; i++) {
+  const [p, l, r] = input[i].trim().split(" ");
+  tree[p] = [l, r];
+}
+
+// console.log(tree)
+
+let pre = "";
+function preorder(node) {
+  if (node === ".") return;
+
+  pre += node; // ✅ "도착하자마자 출력"이라 전위
+  const [l, r] = tree[node];
+  preorder(l);
+  preorder(r);
+}
+
+let ino = "";
+function inorder(node) {
+  if (node === ".") return;
+
+  const [l, r] = tree[node];
+  inorder(l);
+  ino += node; // ✅ "왼쪽 끝까지 보고 나서 출력"이라 중위
+  inorder(r);
+}
+
+let post = "";
+function postorder(node) {
+  if (node === ".") return;
+
+  const [l, r] = tree[node];
+  postorder(l);
+  postorder(r);
+  post += node; // ✅ "자식 둘 다 끝난 뒤 출력"이라 후위
+}
+
+preorder("A");
+inorder("A");
+postorder("A");
+
+console.log(pre);
+console.log(ino);
+console.log(post);
