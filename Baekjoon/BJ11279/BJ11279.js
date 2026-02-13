fb596adf9a604038a0ac056ec5b37b947ee413df
an seungji <ansj8777@gmail.com>
2026-02-13 15:39:15 +0900

    11주 (2/9~2/15): 우선순위 큐 + 힙(Heap): 1번 문제

---
+++ Baekjoon/BJ11279/BJ11279.js
@@ -0,0 +1,14 @@
+/**
+ * 문제 번호: BJ11279
+ * 문제 링크: https://www.acmicpc.net/problem/11279
+ */
+
+const fs = require("fs");
+// 로컬 테스트용 (input.txt 파일 사용)
+const input = fs
+  .readFileSync("Baekjoon/BJ11279/input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+// 백준 제출용 (표준 입력 사용)
+// const input = fs.readFileSync(0, "utf-8").toString().trim().split("\n");
