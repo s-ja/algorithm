fab54b9c4abf543f16a56acdee71c1a5bcf030bd
an seungji <ansj8777@gmail.com>
2026-02-14 12:02:33 +0900

    11주 (2/9~2/15): 우선순위 큐 + 힙(Heap): 1번 문제

--- Baekjoon/BJ11279/BJ11279.js
+++ Baekjoon/BJ11279/BJ11279.js
@@ -12,3 +12,22 @@ const input = fs
   .split("\n");
 // 백준 제출용 (표준 입력 사용)
 // const input = fs.readFileSync(0, "utf-8").toString().trim().split("\n");
+
+const N = Number(input[0]);
+const numbers = input.slice(1).map(Number);
+
+const result = [];
+const heap = [];
+
+for (let i = 0; i < N; i++) {
+  if (numbers[i] === 0) {
+    // heap에서 가장 큰 값을 제거하고 결과 배열에 추가
+    result.push(heap.pop() || 0);
+  } else {
+    // heap에 값을 추가
+    heap.push(numbers[i]);
+    // heap을 정렬
+    heap.sort((a, b) => a - b);
+  }
+}
+console.log(result.join("\n"));
