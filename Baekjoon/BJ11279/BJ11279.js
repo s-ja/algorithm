8790de71dbc21b38848c6d9b7fee5b9c1ec3382c
an seungji <ansj8777@gmail.com>
2026-02-14 15:18:09 +0900

    11주 (2/9~2/15): 우선순위 큐 + 힙(Heap): 1번 문제

--- Baekjoon/BJ11279/BJ11279.js
+++ Baekjoon/BJ11279/BJ11279.js
@@ -16,18 +16,62 @@ const input = fs
 const N = Number(input[0]);
 const numbers = input.slice(1).map(Number);
 
-const result = [];
-const heap = [];
+class MaxHeap {
+  constructor() {
+    this.arr = [];
+  }
 
-for (let i = 0; i < N; i++) {
-  if (numbers[i] === 0) {
-    // heap에서 가장 큰 값을 제거하고 결과 배열에 추가
-    result.push(heap.pop() || 0);
-  } else {
-    // heap에 값을 추가
-    heap.push(numbers[i]);
-    // heap을 정렬
-    heap.sort((a, b) => a - b);
+  size() {
+    return this.arr.length;
+  }
+
+  push(x) {
+    const a = this.arr;
+    a.push(x);
+    let i = a.length - 1;
+
+    while (i > 0) {
+      const p = Math.floor((i - 1) / 2);
+      if (a[p] >= a[i]) break;
+      [a[p], a[i]] = [a[i], a[p]];
+      i = p;
+    }
+  }
+
+  pop() {
+    const a = this.arr;
+    const n = a.length;
+    if (n === 0) return 0;
+    if (n === 1) return a.pop();
+
+    const top = a[0];
+    a[0] = a.pop();
+
+    let i = 0;
+    while (true) {
+      const l = 2 * i + 1;
+      const r = 2 * i + 2;
+      let largest = i;
+
+      if (l < a.length && a[l] > a[largest]) largest = l;
+      if (r < a.length && a[r] > a[largest]) largest = r;
+
+      if (largest === i) break;
+      [a[i], a[largest]] = [a[largest], a[i]];
+      i = largest;
+    }
+
+    return top;
   }
 }
+
+const heap = new MaxHeap();
+const result = [];
+
+for (let i = 0; i < N; i++) {
+  const x = numbers[i];
+  if (x === 0) result.push(heap.pop());
+  else heap.push(x);
+}
+
 console.log(result.join("\n"));
