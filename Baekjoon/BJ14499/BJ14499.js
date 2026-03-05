7683f14c051b66b279b5e9da254d2c0c33ed558e
an seungji <ansj8777@gmail.com>
2026-03-05 15:55:35 +0900

    13주 (2/23~2/28): 시뮬레이션 및 종합 문제: 2번 문제

---
+++ Baekjoon/BJ14499/BJ14499.js
@@ -0,0 +1,14 @@
+/**
+ * 문제 번호: BJ14499
+ * 문제 링크: https://www.acmicpc.net/problem/14499
+ */
+
+const fs = require("fs");
+// 로컬 테스트용 (input.txt 파일 사용)
+const input = fs
+  .readFileSync("Baekjoon/BJ14499/input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+// 백준 제출용 (표준 입력 사용)
+// const input = fs.readFileSync(0, "utf-8").toString().trim().split("\n");
